1e4da5c41d26479937d0cf14f0466c39380cf28f
Sohel Rana <73012378+fsmdsohel@users.noreply.github.com>
2021-08-03 21:45:41 +0600

    New problem solved

---
+++ main.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", () => {
+  document
+    .querySelector("#intro_image img")
+    .classList.add("img-fluid bike_animation");
+  //   setTimeout(() => {
+  //   }, 1000);
+});
